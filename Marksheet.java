3b3276605e2a06ded988e32ad6dcb2f4642bd367
PandaAnshuman <119730001+PandaAnshuman@users.noreply.github.com>
2022-12-03 14:43:06 +0530

    Add files via upload

---
+++ Marksheet.java
@@ -0,0 +1,51 @@
+
+public class Marksheet {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("| ___________________________________________________________________________________|");
+		System.out.println("|                           S I K S H A 'O' A N U S A N D H A N                      |");
+		System.out.println("|                                (DEEMED TO BE UNIVERSITY)                           |");                   
+		System.out.println("|                           Re-Accredited by NAAC with 'A' Grade                     |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|                                 G R A D E  S H E E T                               |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|  EXAMINATION     :FIRST SEMESTER EXAMINATION, 2023                                 |");
+		System.out.println("|  COURSE          :BACHELOR OF TECHNOLOGY,COMPUTER SCIENCE AND ENGINEERING          |");
+		System.out.println("|  INSTITUTION     :INSTITUION OF TECHNICAL EDUCATION AND RESEARCH                   |");
+		System.out.println("|  NAME            :ANSHUMAN PANDA                                                   |");
+		System.out.println("|  REGISTRATION NO.:2241016053                                                       |");
+		System.out.println("|  MEDIUM OF PAPER :ENGLISH                                                          |");
+		System.out.println("|  _____________________________________________________________________________     |");
+		System.out.println("| |SL NO.|                    PAPER NAME                  |GRADE POINT | GRADE  |    |");
+		System.out.println("| |  1.  |INTRODUCTION TO COMP PROGRAMMING                |    9.5     |   O    |    |");
+		System.out.println("| |  2.  |UNIVERSITY PHYSICS MECHANICS                    |    9.0     |   O    |    |");
+		System.out.println("| |  3.  |CALCULUS (A)                                    |    8.5     |   A    |    |");
+		System.out.println("| |  4.  |DISCRETE MATHEMATICS                            |    8.5     |   A    |    |");
+		System.out.println("| |  5.  |COMMUNICATIONS AND CRITICAL THINKING            |    8.0     |   A    |    |");
+		System.out.println("| |______|________________________________________________|____________|________|    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|  OVERALL GRADE POINT : 8.7                                OVERALL GRADE : A        |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("|SIGNATURE OF PARENTS               SIGNATURE OF HOD               SIGNATURE OF DEAN |");
+		System.out.println("|____________________________________________________________________________________|");
+        System.out.println("|                                                                                    |");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+
+	   
+		
+		
+		
+		
+		
+	}
+
+}
